f3b072206bc19b60e43137d880099c16f6cf35c7
mohammedmosaad <mohammedmosaad869@gmail.com>
2024-09-08 20:34:59 +0300

    create home local data source

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -2,7 +2,6 @@ import 'package:bookly_clean/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly_clean/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
-
 import '../../../../core/use_cases/use_case.dart';
 
 class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParams> {
