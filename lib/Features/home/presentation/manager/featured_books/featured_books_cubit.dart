e327d973cb276461087cbd3b6cd4f5994152b0fb
mohammedmosaad <mohammedmosaad869@gmail.com>
2024-09-12 23:49:46 +0300

    implement cubit fetch feaatured books method

--- lib/Features/home/presentation/manager/featured_books/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books/featured_books_cubit.dart
@@ -1,10 +1,19 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
+import 'package:bookly_clean/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../domain/entities/book_entity.dart';
-
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await featuredBooksUseCase.call();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(message: failure.message)),
+      (books) => emit(FeaturedBooksSuccess(books: books)),
+    );
+  }
 }
